74262bff82c7d8a00b05b2d024a593aa3845c20b
71104 <a71104@gmail.com>
2026-07-20 12:43:31 +0200

    Refactoring.

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ use primitive_types::U256;
 use std::fs::File;
 use std::io::Write;
 
-mod bls12_381;
-mod bluesky;
+mod v2 {
+    pub mod bls12_381;
+    pub mod bluesky;
+}
+
 mod utils;
 
 const NUM_ROUNDS: usize = 8 + 56;
@@ -24,22 +27,22 @@ fn write_constants<const N: usize, const M: usize>(
     Ok(())
 }
 
-fn write_constants_t3() -> Result<()> {
-    write_constants::<NUM_ROUNDS, 3>(&*bls12_381::RC3, "out/arc_t3.bin")?;
-    write_constants::<3, 3>(&*bls12_381::FL3, "out/fl_t3.bin")?;
-    write_constants::<3, 3>(&*bls12_381::PL3, "out/pl_t3.bin")?;
+fn write_constants_v2_t3() -> Result<()> {
+    write_constants::<NUM_ROUNDS, 3>(&*v2::bls12_381::RC3, "out/v2/arc_t3.bin")?;
+    write_constants::<3, 3>(&*v2::bls12_381::FL3, "out/v2/fl_t3.bin")?;
+    write_constants::<3, 3>(&*v2::bls12_381::PL3, "out/v2/pl_t3.bin")?;
     Ok(())
 }
 
-fn write_constants_t4() -> Result<()> {
-    write_constants::<NUM_ROUNDS, 4>(&*bls12_381::RC4, "out/arc_t4.bin")?;
-    write_constants::<4, 4>(&*bls12_381::FL4, "out/fl_t4.bin")?;
-    write_constants::<4, 4>(&*bls12_381::PL4, "out/pl_t4.bin")?;
+fn write_constants_v2_t4() -> Result<()> {
+    write_constants::<NUM_ROUNDS, 4>(&*v2::bls12_381::RC4, "out/v2/arc_t4.bin")?;
+    write_constants::<4, 4>(&*v2::bls12_381::FL4, "out/v2/fl_t4.bin")?;
+    write_constants::<4, 4>(&*v2::bls12_381::PL4, "out/v2/pl_t4.bin")?;
     Ok(())
 }
 
 fn main() -> Result<()> {
-    write_constants_t3()?;
-    write_constants_t4()?;
+    write_constants_v2_t3()?;
+    write_constants_v2_t4()?;
     Ok(())
 }
